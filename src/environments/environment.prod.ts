d9c2d56f62728b4fb6bae628561f3535a421f739
Evgeniya Derevygo <evgeniya.nice@yandex.by>
2021-09-19 01:56:03 +0300

    feat: enable import api, add styles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,14 +5,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://i9avghzhe6.execute-api.eu-west-1.amazonaws.com/dev/import',
     bff: 'https://i6big9ofob.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
-    product: false,
+    product: true,
     order: false,
-    import: false,
+    import: true,
     bff: false,
     cart: false,
   },
