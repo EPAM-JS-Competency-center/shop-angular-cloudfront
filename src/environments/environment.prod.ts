ae14aff4faa2c5955cbeab6aba6518e5dc0a69b4
Evgeniya Derevygo <evgeniya.nice@yandex.by>
2021-10-25 20:11:36 +0300

    feat: update cart api variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,13 +7,13 @@ export const environment: Config = {
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://i9avghzhe6.execute-api.eu-west-1.amazonaws.com/dev',
     bff: ' https://s3xahfblr8.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'http://evgender-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
     import: true,
-    bff: false,
-    cart: false,
+    bff: true,
+    cart: true,
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,15 +10,15 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://i9avghzhe6.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: ' https://s3xahfblr8.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://s3xahfblr8.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'http://evgender-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
     import: true,
-    bff: false,
-    cart: false,
+    bff: true,
+    cart: true,
   },
 };
 
