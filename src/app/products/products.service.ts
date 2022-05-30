f48798d126df47a0d9b04b04e8e5bbdeebb6d262
Oleksandr Bondarenko <oleksandr_bondarenko2@epam.com>
2022-05-30 16:22:00 +0300

    feat: serverless lab

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -56,14 +56,14 @@ export class ProductsService extends ApiService {
   }
 
   getProducts(): Observable<Product[]> {
-    if (!this.endpointEnabled('bff')) {
+    if (!this.endpointEnabled('product')) {
       console.warn(
         'Endpoint "bff" is disabled. To enable change your environment.ts config'
       );
       return this.http.get<Product[]>('/assets/products.json');
     }
 
-    const url = this.getUrl('bff', 'products');
+    const url = this.getUrl('product', 'products');
     return this.http.get<Product[]>(url);
   }
 
