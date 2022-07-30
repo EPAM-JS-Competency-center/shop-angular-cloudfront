faf1a6545c77b4be6f7afc25f4a89a0053a44c89
Ivan Radchenko <ivan_radchenko@epam.com>
2022-07-30 16:04:09 +0300

    feat: [TASK5] Integration with S3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,14 +5,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://lfh6re2wfg.execute-api.eu-west-1.amazonaws.com/dev/',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://64qafup6a2.execute-api.eu-west-1.amazonaws.com/dev/',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
-    import: false,
+    import: true,
     bff: false,
     cart: false,
   },
