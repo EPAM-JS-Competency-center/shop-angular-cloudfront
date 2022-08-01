715a1cedaa8e78aaddf277b1e74990f580a79d82
Max <mlalx@tut.by>
2022-08-01 11:20:53 +0200

    feat: update cart api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment: Config = {
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://0xcdezs5t3.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://0rzafcr3yh.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'http://max-sv-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
   },
   apiEndpointsEnabled: {
     product: true,
