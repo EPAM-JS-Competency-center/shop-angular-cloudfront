96177585e912878cbc3bacb68f95df47189e28db
Иван Меньщиков <ivanmensikov@MacBook-Air-Ivan.local>
2022-08-04 11:06:22 +0200

    Updated config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://2l4x2gj2j9.execute-api.us-east-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://f03v7y1a3m.execute-api.us-east-1.amazonaws.com/dev',
     bff: 'https://2l4x2gj2j9.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
