5e7b87dd7eb3a54142930e2b888a63df2a782f7f
Dzianis Puchko <dixon.ltd50@gmail.com>
2022-09-19 11:04:08 +0200

    feat: added products endpoint to the frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,14 +7,14 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://3w5dmw8tz3.execute-api.eu-west-1.amazonaws.com/dev/',
+    order: 'https://3w5dmw8tz3.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://3w5dmw8tz3.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://3w5dmw8tz3.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'https://3w5dmw8tz3.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
-    product: false,
+    product: true,
     order: false,
     import: false,
     bff: false,
