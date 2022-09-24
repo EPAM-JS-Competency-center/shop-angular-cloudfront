9afd700ee2deffd5c8335f4e38eb7fc6b68966ea
aliaksei.kamoska <zaliakseik@pttep.com>
2022-09-24 20:21:52 +0500

    feat: enable product endpoint in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,14 +7,14 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://b8nbu70yzh.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
-    product: false,
+    product: true,
     order: false,
     import: false,
     bff: false,
