1b7c5cf6f1d39ba9196ee23da93901afa788db7d
Dzianis Puchko <dixon.ltd50@gmail.com>
2022-10-03 13:00:28 +0200

    feat: task-5 added import endpoint to frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://cunkdv3lsa.execute-api.eu-west-1.amazonaws.com/dev/',
     order: 'https://cunkdv3lsa.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://cunkdv3lsa.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://8bx3k8km89.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://cunkdv3lsa.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://cunkdv3lsa.execute-api.eu-west-1.amazonaws.com/dev',
   },
