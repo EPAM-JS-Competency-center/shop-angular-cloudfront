2a0f54c4fb8c3ff11700ff5a2bfb9fd7990aa1e0
Dmytro Pavlov <dmytro.pavlov@wickes.co.uk>
2022-10-12 11:33:02 +0300

    TASK-3: change domain for get all products

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -60,7 +60,9 @@ export class ProductsService extends ApiService {
       console.warn(
         'Endpoint "bff" is disabled. To enable change your environment.ts config'
       );
-      return this.http.get<Product[]>('/assets/products.json');
+      return this.http.get<Product[]>(
+        'https://kywx8j0mt4.execute-api.eu-west-1.amazonaws.com/dev/products'
+      );
     }
 
     const url = this.getUrl('bff', 'products');
