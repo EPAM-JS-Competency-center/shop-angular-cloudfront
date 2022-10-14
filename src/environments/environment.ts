87afe7908a6a5b40a7286f49d61704908361957e
Stashi Ku <kurushinstas@gmail.com>
2022-10-14 14:37:38 +0300

    AWS-FE-TASK-4: intergration with NoSQL db

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://j6cqnx5eq5.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://arq0plgxm6.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
