7cb4beaa4436f853734d6d4b9e20909d8c451297
Pavel Kuzniatsou <pavel.kusnecov@gmail.com>
2022-10-16 10:17:57 +0200

    feat: dynamodb

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://8mhm1ygabg.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://uv28qxefda.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
