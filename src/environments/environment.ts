fae12d4c4c9be91152692fbe25e770c90f286518
Stashi Ku <kurushinstas@gmail.com>
2022-10-16 16:10:17 +0300

    AWS-FE-Task-5: Intergration with S3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://rr6vcmvbp1.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://arq0plgxm6.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
