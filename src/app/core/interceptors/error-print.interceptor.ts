b41d9454f34f28756179f4224bc5648404be8140
aliaksei.kamoska <aliaksei.kamoska@ihsmarkit.com>
2022-10-23 16:26:23 +0500

    feat: add display alerts for the responses in 401 and 403 HTTP statuses

--- src/app/core/interceptors/error-print.interceptor.ts
+++ src/app/core/interceptors/error-print.interceptor.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {
+  HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
@@ -13,21 +14,43 @@ import { tap } from 'rxjs/operators';
 export class ErrorPrintInterceptor implements HttpInterceptor {
   constructor(private readonly notificationService: NotificationService) {}
 
-  intercept(
+  public intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       tap({
-        error: () => {
+        error: (error: unknown) => {
           const url = new URL(request.url);
+          const { status } = error as HttpErrorResponse;
+          let errorMessage = '';
 
-          this.notificationService.showError(
-            `Request to "${url.pathname}" failed. Check the console for the details`,
-            0
-          );
+          switch (status) {
+            case 401:
+              errorMessage = this.createUnauthorizedMessage();
+              break;
+            case 403:
+              errorMessage = this.createForbiddenMessage();
+              break;
+            default:
+              errorMessage = this.createDefaultErrorMessage(url.pathname);
+          }
+
+          this.notificationService.showError(errorMessage, 0);
         },
       })
     );
+  }
+
+  private createUnauthorizedMessage(): string {
+    return 'You are not authorized. Please, provide your credentials';
+  }
+
+  private createForbiddenMessage(): string {
+    return 'Your acces to resource forbidden. Check your credential details';
+  }
+
+  private createDefaultErrorMessage(url: string): string {
+    return `Request to "${url}" failed. Check the console for the details`;
   }
 }
