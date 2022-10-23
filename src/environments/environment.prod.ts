3342b5d06ab3568e944bb0dfe73d8fb8c7090790
dzianis danilkou <d.danilkov@dialmyapp.com>
2022-10-23 22:58:18 +0300

    wip task3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,14 +5,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://xrrzo5hore.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://xwd18eaflg.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
