7e512853ca2ba64e3834229b15e009be99d8a7c6
Pavel Kuzniatsou <pavel.kusnecov@gmail.com>
2022-10-30 17:11:00 +0100

    feat: integration with s3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://1cm3igs7r1.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://uv28qxefda.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
