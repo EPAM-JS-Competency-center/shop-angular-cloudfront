0393769c8d07926991ea9a18bb4519d16451ae91
Hellsingi <il.dzemchanka@gmail.com>
2022-11-01 19:13:49 +0100

    feat: add env for card

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,13 +7,13 @@ export const environment: Config = {
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://kfb1wfjj8c.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://ei5jgu2ft4.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'https://6n1wnohoii.execute-api.eu-west-1.amazonaws.com/dev/',
   },
   apiEndpointsEnabled: {
-      product: false,
+    product: false,
     order: false,
     import: true,
     bff: false,
-    cart: false,
+    cart: true,
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,14 +11,14 @@ export const environment: Config = {
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://kfb1wfjj8c.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://ei5jgu2ft4.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'https://6n1wnohoii.execute-api.eu-west-1.amazonaws.com/dev/',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
     import: true,
     bff: true,
-    cart: false,
+    cart: true,
   },
 };
 
