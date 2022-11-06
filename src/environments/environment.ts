85f554d75c4a24072ae5f1da80d0387e7c7c51e9
aliaksei.kamoska <aliaksei.kamoska@ihsmarkit.com>
2022-11-06 19:47:27 +0500

    feat: update cart api endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment: Config = {
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://xp6ffu9k55.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'http://avkomosko-cart-api-dev.eu-west-1.elasticbeanstalk.com/api/profile/cart',
   },
   apiEndpointsEnabled: {
     product: true,
