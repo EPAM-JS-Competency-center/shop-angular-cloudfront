d9c5dfbcab557f9e90d8fd51f4f6f71a4f83d398
aliaksei.kamoska <aliaksei.kamoska@ihsmarkit.com>
2022-11-13 20:19:04 +0500

    feat: add bff url to env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,14 +6,14 @@ export const environment: Config = {
     product: 'https://b8nbu70yzh.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://xp6ffu9k55.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://avkomosko-bff-api-dev.eu-west-1.elasticbeanstalk.com',
     cart: 'http://avkomosko-cart-api-dev.eu-west-1.elasticbeanstalk.com/api/profile/cart',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
     import: true,
-    bff: false,
-    cart: false,
+    bff: true,
+    cart: true,
   },
 };
