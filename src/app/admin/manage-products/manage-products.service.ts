a1968ab3c761f8795578f9cc95c0d2ce8fd88696
bogdan-panteleev <panteleevbbbogdan@gmail.com>
2022-11-16 14:51:47 +0300

    integrate with cognito

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -2,11 +2,33 @@ import { Injectable, Injector } from '@angular/core';
 import { EMPTY, Observable } from 'rxjs';
 import { ApiService } from '../../core/api.service';
 import { switchMap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable()
 export class ManageProductsService extends ApiService {
-  constructor(injector: Injector) {
+  constructor(injector: Injector, private route: ActivatedRoute) {
     super(injector);
+  }
+
+  getAuthToken() {
+    return this.http.post(
+      'https://bahdan-pantsialeyeu.auth.eu-central-1.amazoncognito.com/oauth2/token',
+      {},
+      {
+        params: {
+          grant_type: 'authorization_code',
+          client_id: '1sbf1bbef5endbe7fabbqovgap',
+          redirect_uri: 'http://localhost:4200/admin/products',
+          code: this.route.snapshot.queryParams.code,
+        },
+        headers: {
+          authorization:
+            'Basic MXNiZjFiYmVmNWVuZGJlN2ZhYmJxb3ZnYXA6bjduZmd0czNpaGhndHFrcWlpMDJpbmhqZDlqb2k2NThqc3RrZTBwbTR2ZWthaWt2cTUx',
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    );
   }
 
   uploadProductsCSV(file: File): Observable<unknown> {
@@ -30,8 +52,8 @@ export class ManageProductsService extends ApiService {
         fileName,
       },
       headers: {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        Authorization: localStorage.getItem('auth-token') || 'nothing',
+        authorization: JSON.parse(localStorage.getItem('auth-data') || '{}')
+          .id_token,
       },
     });
   }
