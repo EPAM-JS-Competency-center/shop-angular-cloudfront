a70b4c63e3b83700ca5bf10f1fef59efc700906a
Adrian Kotlinski <Adrian_Kotlinski@epam.com>
2023-03-12 10:36:04 +0100

    reverse changes in environment.ts file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,17 +3,17 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://jj9rtvbo32.execute-api.us-east-1.amazonaws.com/dev',
+    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://vsdtk4z9ij.execute-api.us-east-1.amazonaws.com/dev',
-    bff: 'https://jj9rtvbo32.execute-api.us-east-1.amazonaws.com/dev',
+    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
-    product: true,
+    product: false,
     order: false,
-    import: true,
-    bff: true,
+    import: false,
+    bff: false,
     cart: false,
   },
 };
