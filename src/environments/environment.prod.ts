de3b45054f499d2f3cebdeed0164225b7377b56c
Erwin Bustillo <erwinb1996@outlook.com>
2023-03-13 11:24:12 -0500

    updated import lambda

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,14 +5,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://hl2hom669k.execute-api.us-east-1.amazonaws.com',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://hndl3a1rj7.execute-api.us-east-1.amazonaws.com',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
-    import: false,
+    import: true,
     bff: false,
     cart: false,
   },
