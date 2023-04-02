a8d7f625d6ddac48c821f8baacf72f5a987e498d
Dzmitry_Yarash <dzmitry_yarash@epam.com>
2023-04-03 00:42:35 +0300

    feat: modify product-service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,17 +7,17 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://k4bw7kw927.execute-api.us-east-1.amazonaws.com/dev',
+    order: 'https://.execute-api.us-east-1.amazonaws.com/dev',
+    import: 'https://.execute-api.us-east-1.amazonaws.com/dev',
+    bff: 'https://k4bw7kw927.execute-api.us-east-1.amazonaws.com/dev',
+    cart: 'https://.execute-api.us-east-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
-    product: false,
+    product: true,
     order: false,
     import: false,
-    bff: false,
+    bff: true,
     cart: false,
   },
 };
