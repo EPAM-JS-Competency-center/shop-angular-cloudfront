fc8993bd7c06764e49099e38b5fbe6da7dc1d3ed
oldjunior <anton.b.utkin@gmail.com>
2023-04-09 13:35:20 +0300

    Module 3 tasks: First API with AWS API Gateway and AWS Lambda
    * change region
    * change api endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://1r4h0736me.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://yk2pxqywh5.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
