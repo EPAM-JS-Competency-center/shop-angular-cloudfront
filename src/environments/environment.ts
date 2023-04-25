b3e97a216cc12c57b8cd0c888528c4e14c500677
yahor_barysevich <yahor_barysevich@epam.com>
2023-04-25 14:21:13 +0300

    hometask-5: integrate import api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://0yuyb78yfi.execute-api.eu-west-1.amazonaws.com/dev/products',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://1zrxitoyt5.execute-api.us-east-1.amazonaws.com/dev/import',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
-    import: false,
+    import: true,
     bff: false,
     cart: false,
   },
