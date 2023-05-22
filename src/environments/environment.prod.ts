767223d0a080d47a3d3053c2b52b148e81d459c6
Alvaro Mazuera U <am@alvaromazuera.com>
2023-05-22 14:59:10 -0500

    :ok_hand: Fix production env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'http://localhost:3000/dev',
-    order: 'http://localhost:3000/dev',
-    import: 'http://localhost:3000/dev',
-    bff: 'http://localhost:3000/dev',
-    cart: 'http://localhost:3000/dev',
+    product: '/api',
+    order: '/api',
+    import: '/api',
+    bff: '/api',
+    cart: '/api',
   },
   apiEndpointsEnabled: {
     product: true,
