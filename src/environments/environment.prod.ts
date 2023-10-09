6014e644b616f287c18e9740338b70091058a8ab
Berkin_Duz <berkin_duz@epam.com>
2023-10-09 22:24:56 +0300

    task-3 completed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,17 +3,17 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://z2ft6yinia.execute-api.us-east-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://z2ft6yinia.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
-    product: false,
+    product: true,
     order: false,
     import: false,
-    bff: false,
+    bff: true,
     cart: false,
   },
 };
