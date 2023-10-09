2d80468ef76d85e5210e6c14e9fbc85afaecc2e6
Serhat KAYA <serhat_kaya@epam.com>
2023-10-09 22:22:32 +0300

    task-3 done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,17 +7,17 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://oar3zwb2jg.execute-api.us-east-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    apiBase: 'https://oar3zwb2jg.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
     import: false,
-    bff: false,
+    apiBase: true,
     cart: false,
   },
 };
