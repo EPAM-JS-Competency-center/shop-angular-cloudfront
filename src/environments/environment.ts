52f2a9e4e3b8c4a81271d887e818119d97328d81
Maria Korneeva <mkorneeva@infoblox.com>
2023-10-15 14:59:10 +0300

    feat: update endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://zfhfqqao2e.execute-api.us-east-1.amazonaws.com/dev/',
+    product: 'https://nylkuwzyk5.execute-api.us-east-1.amazonaws.com/dev/',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
