066d3c7c4537986798867cf6da0ab3f43a90332a
Artsiom_Murashko <Artsiom_Murashko@epam.com>
2023-10-24 15:26:22 +0300

    refactor: activate import routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,14 +5,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://16sce1q0m3.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://n5p4x5lvvg.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://16sce1q0m3.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://n5p4x5lvvg.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
