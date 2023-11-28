a6e938e0f938f3e302063125691791c6dccbfaf2
Uladzimir Pantsiukhou <UPantsiukhou@slb.com>
2023-11-28 14:03:50 +0100

    feat: host app from blob storage account

--- main.tf
+++ main.tf
@@ -18,3 +18,17 @@ resource "azurerm_resource_group" "front_end_rg" {
     name     = "rg-frontend-sand-ne-001"
     location = "northeurope"
 }
+
+resource "azurerm_storage_account" "front_end_storage_account" {
+  name                     = "stgsandfrontendne001"
+  location                 = "northeurope"
+
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  resource_group_name      = azurerm_resource_group.front_end_rg.name
+
+  static_website {
+    index_document = "index.html"
+  }
+}
