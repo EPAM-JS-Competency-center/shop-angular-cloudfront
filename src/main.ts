f7ad91d9d6f9776331167c6ad18687ef319be4d2
Sergey Gultyayev <gultyayev.sergey@gmail.com>
2024-03-11 19:51:00 +0100

    refactor: migrate to standalone components #4

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,10 @@
-import { enableProdMode, importProvidersFrom, Provider } from '@angular/core';
+import { importProvidersFrom, Provider } from '@angular/core';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatBadgeModule } from '@angular/material/badge';
-import { CartModule } from './app/cart/cart.module';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app/app-routing.module';
-import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { CONFIG_TOKEN } from './app/core/injection-tokens/config.token';
 import { ErrorPrintInterceptor } from './app/core/interceptors/error-print.interceptor';
 import {
@@ -29,24 +21,9 @@ const interceptors: Provider[] = [
   },
 ];
 
-if (environment.production) {
-  enableProdMode();
-}
-
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(
-      BrowserModule,
-      AppRoutingModule,
-      MatToolbarModule,
-      MatButtonModule,
-      MatIconModule,
-      MatMenuModule,
-      MatTooltipModule,
-      CartModule,
-      MatBadgeModule,
-      MatSnackBarModule,
-    ),
+    importProvidersFrom(AppRoutingModule),
     interceptors,
     {
       provide: CONFIG_TOKEN,
